6f6779a1e0f5b9af6e907ffa635e8e2e83ca4d4e
henrique expedito <henriqueexpedito964@gmail.com>
2025-07-23 11:42:29 -0300

    modified:   css/style.css new file:   img/container-jogo/capa-poppy-playtime-4.png modified:   index.html new file:   js/excluir-plataforma.js modified:   js/poppy-selecionar-plataforma.js deleted:    js/remover-button.js

--- js/remover-button.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll('.btn-plataforma ul li a');
-
-buttons.forEach((button) => {
-  if (button.href === '#' || button.href === '') {
-    button.parentNode.parentNode.remove();
-  }
-});
